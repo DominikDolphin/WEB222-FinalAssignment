d45e4039a468bd7f1def06d0ab7f4a9d3256c424
DominikDolphin <dominik.thibaudeau@gmail.com>
2020-04-07 16:13:09 -0400

    Added footer and reflection

--- imageData.js
+++ imageData.js
@@ -13,29 +13,58 @@ var imgData = [{
         Price: 19.98
     },
     {
-        Name: "Calgary Flames",
-        Description: "Adidas Calgary Flames Hat",
+        Name: "Vancouver Canucks",
+        Description: "Fanatics' Vancouver Canucks Hat",
         availableSize: 'XS/S/M/L',
         Regular: 29.99,
         Price: 19.98
     },
     {
-        Name: "Calgary Flames",
-        Description: "Adidas Calgary Flames Hat",
+        Name: "TOR Maple Leafs",
+        Description: "Fanatics' Toronto Maple Leafs Hat",
         availableSize: 'XS/S/M/L',
         Regular: 29.99,
         Price: 19.98
     },
     {
-        Name: "Calgary Flames",
-        Description: "Adidas Calgary Flames Hat",
+        Name: "Edmonton Oilers",
+        Description: "Fanatics' Edmonton Oilers Hat",
         availableSize: 'XS/S/M/L',
         Regular: 29.99,
         Price: 19.98
     },
     {
-        Name: "Calgary Flames",
-        Description: "Adidas Calgary Flames Hat",
+        Name: "Ottawa Senators",
+        Description: "Adidas Ottawa Senators Hat",
+        availableSize: 'XS/S/M/L',
+        Regular: 29.99,
+        Price: 19.98
+    },
+    {
+        Name: "NY Islanders",
+        Description: "Fanatics' New York Islanders Hat",
+        availableSize: 'XS/S/M/L',
+        Regular: 29.99,
+        Price: 19.98
+    },
+
+    {
+        Name: "Team Canada",
+        Description: "Nike Team Canada Hat",
+        availableSize: 'XS/S/M/L',
+        Regular: 29.99,
+        Price: 19.98
+    },
+    {
+        Name: "Tor St. Pats",
+        Description: "Fanatics' Toronto St. Pats Hat",
+        availableSize: 'XS/S/M/L',
+        Regular: 29.99,
+        Price: 19.98
+    },
+    {
+        Name: "Vegas Golden Knights",
+        Description: "Fanatics' Vegas Golden Knight's Hat",
         availableSize: 'XS/S/M/L',
         Regular: 29.99,
         Price: 19.98
